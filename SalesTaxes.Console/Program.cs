14d3a4a10b6c48618e5e42920ee9051becc7c029
Simon Ehleringer <simonehleringer@gmail.com>
2021-11-02 20:24:32 +0100

    Add project files.

---
+++ SalesTaxes.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesTaxes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

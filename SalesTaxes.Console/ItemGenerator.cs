03078e8085df05b4fb3818a47639a42b33c90a12
Simon Ehleringer <simonehleringer@gmail.com>
2021-11-03 19:30:45 +0100

    Add code in main method to print out the test cases

---
+++ SalesTaxes.Console/ItemGenerator.cs
@@ -0,0 +1,121 @@
+﻿using SalesTaxes.Core.Enums;
+using SalesTaxes.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesTaxes.ConsoleApp
+{
+    public static class ItemGenerator
+    {
+        public static IList<Item> GetFirstItems()
+        {
+            var item1 = new Item()
+            {
+                Category = Category.Books,
+                IsImported = false,
+                Name = "Book",
+                PriceWithoutTax = 12.49M
+            };
+
+            var item2 = new Item()
+            {
+                Category = Category.ElectronicProducts,
+                IsImported = false,
+                Name = "Music CD",
+                PriceWithoutTax = 14.99M
+            };
+
+            var item3 = new Item()
+            {
+                Category = Category.Food,
+                IsImported = false,
+                Name = "Chocolate bar",
+                PriceWithoutTax = 0.85M
+            };
+
+            var items = new List<Item>
+            {
+                item1,
+                item2,
+                item3
+            };
+
+            return items;
+        }
+
+        public static IList<Item> GetSecondItems()
+        {
+            var item1 = new Item()
+            {
+                Category = Category.Food,
+                IsImported = true,
+                Name = "Imported box of chocolates",
+                PriceWithoutTax = 10.00M
+            };
+
+            var item2 = new Item()
+            {
+                Category = Category.CosmeticProducts,
+                IsImported = true,
+                Name = "Imported bottle of perfume",
+                PriceWithoutTax = 47.50M
+            };
+
+            var items = new List<Item>
+            {
+                item1,
+                item2
+            };
+
+            return items;
+        }
+
+        public static IList<Item> GetThirdItems()
+        {
+            var item1 = new Item()
+            {
+                Category = Category.CosmeticProducts,
+                IsImported = true,
+                Name = "Imported bottle of perfume",
+                PriceWithoutTax = 27.99M
+            };
+
+            var item2 = new Item()
+            {
+                Category = Category.CosmeticProducts,
+                IsImported = false,
+                Name = "Bottle of perfume",
+                PriceWithoutTax = 18.99M
+            };
+
+            var item3 = new Item()
+            {
+                Category = Category.MedicalProducts,
+                IsImported = false,
+                Name = "Packet of headache pills",
+                PriceWithoutTax = 9.75M
+            };
+
+            var item4 = new Item()
+            {
+                Category = Category.Food,
+                IsImported = true,
+                Name = "Imported box of chocolates",
+                PriceWithoutTax = 11.25M
+            };
+
+            var items = new List<Item>
+            {
+                item1,
+                item2,
+                item3,
+                item4
+            };
+
+            return items;
+        }
+    }
+}
--- SalesTaxes.Console/Program.cs
+++ SalesTaxes.Console/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using SalesTaxes.Core.Services.Interfaces;
+using SalesTaxes.Core.Services;
+using SalesTaxes.Core.Models;
+using SalesTaxes.Core.Enums;
+using System.Collections.Generic;
 
 namespace SalesTaxes.ConsoleApp
 {
@@ -6,7 +13,42 @@ namespace SalesTaxes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = CreateHostBuilder(args).Build();
+            var receiptService = host.Services.GetService<IReceiptService>();
+
+            var firstReceipt = receiptService.Create(ItemGenerator.GetFirstItems());
+            PrintReceipt(1, firstReceipt);
+
+            var secondReceipt = receiptService.Create(ItemGenerator.GetSecondItems());
+            PrintReceipt(2, secondReceipt);
+
+            var thirdReceipt = receiptService.Create(ItemGenerator.GetThirdItems());
+            PrintReceipt(3, thirdReceipt);
+        }
+
+        private static void PrintReceipt(int caseNo, Receipt receipt)
+        {
+            Console.WriteLine($"Case {caseNo}");
+
+            foreach (var receiptItem in receipt.ReceiptItems)
+            {
+                Console.WriteLine($"{receiptItem.Name}: {receiptItem.PriceWithTax}");
+            };
+
+            Console.WriteLine($"Sales Taxes: {receipt.SalesTaxes}");
+            Console.WriteLine($"Total: {receipt.TotalPrice}");
+
+            Console.WriteLine(string.Empty);
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<IReceiptItemService, ReceiptItemService>();
+                    services.AddSingleton<IReceiptService, ReceiptService>();
+                });
         }
     }
 }
